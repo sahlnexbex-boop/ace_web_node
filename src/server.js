72ee9dd6ba3b678e72bb2d9d587bf888900dcf3c
sahlnexbex-boop <sahl.nexbex@gmail.com>
2026-01-22 15:29:55 +0530

    index check

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+import "dotenv/config";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -41,7 +42,9 @@ import { startEditorCleanupJob } from "./utils/editorCleanup.job.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+
+
+// dotenv.config();
 
 const app = express();
 app.use(
