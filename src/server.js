8a98d04426218c84a051a96130ed91089571c385
sahlnexbex-boop <sahl.nexbex@gmail.com>
2026-02-20 17:14:45 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -48,13 +48,11 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL
-    ],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(express.json());
